1fdbd42c2212bc8623198af544ce4ef05ff936a0
yc-zhang <turalyon.zhangyc@gmail.com>
2016-09-12 18:45:27 +0800

    Implement the test of base fee.

--- src/main/java/Taxi.java
+++ src/main/java/Taxi.java
@@ -1,6 +1,6 @@
+
 public class Taxi {
-
-
-
-
+    public Float calculateFee(Float distance) {
+        return 1F;
+    }
 }
--- src/test/java/TaxiTest.java
+++ src/test/java/TaxiTest.java
@@ -1,22 +1,33 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.assertEquals;
 
 public class TaxiTest {
 
+    private Taxi taxi;
+
+    @Before
+    public void setUp() {
+        taxi = new Taxi();
+    }
+
     @Test
     public void priceOf0KMShouldBe6() {
-
+        assertEquals((Float) 6F, taxi.calculateFee(0F));
     }
 
     @Test
     public void priceOf1KMShouldBe6() {
-
+        assertEquals((Float) 6F, taxi.calculateFee(1F));
     }
 
     @Test
     public void priceOf2KMShouldBe6() {
-
+        assertEquals((Float) 6F, taxi.calculateFee(2F));
     }
 
 }
