2ee8f4c95cf74c51919f21478142c9155f67361b
yc-zhang <turalyon.zhangyc@gmail.com>
2016-09-12 18:57:08 +0800

    Implement deliver fee.

--- src/main/java/Taxi.java
+++ src/main/java/Taxi.java
@@ -2,8 +2,14 @@
 public class Taxi {
 
     private static final Float BASE_FEE = 6F;
+    private static final Float DELIVER_FEE_PER_KM = 0.8F;
+    private static final Float BASE_DISTANCE = 2F;
 
     public Float calculateFee(Float distance) {
-        return BASE_FEE;
+        Float deliver_fee = 0F;
+        if (distance > BASE_DISTANCE) {
+            deliver_fee = (distance - BASE_DISTANCE) * DELIVER_FEE_PER_KM;
+        }
+        return BASE_FEE + deliver_fee;
     }
 }
